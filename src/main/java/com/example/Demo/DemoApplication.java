b14c5dddbbaf8abfa04c4b96c58092e7b4570f37
Irfon Baig <irfon.java@gmail.com>
2018-09-26 00:27:24 +0530

    Update DemoApplication.java

--- src/main/java/com/example/Demo/DemoApplication.java
+++ src/main/java/com/example/Demo/DemoApplication.java
@@ -8,14 +8,5 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		//adding files in workspace
-		//adding files-1 in repo
-		//adding files-2 in workspace
-		//adding files-3 in repo
-		//adding files-4 in workspace
-		//adding files-5 in workspace
-		//adding files-6 in workspace
-		//adding files-7 in workspace
-		//adding files-8 in workspace
 	}
 }
